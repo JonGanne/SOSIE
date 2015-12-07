4348bcbd218cf527c3d17083aac455b81aabfd89
JonGanne <jonathan.ganne@isty.uvsq.fr>
2015-12-07 11:19:33 +0100

    Ajout table module

--- Base de données/Script.sql
+++ Base de données/Script.sql
@@ -52,6 +52,13 @@ PRIMARY KEY(IdEnseignant),
 FOREIGN KEY(IdUtilisateur) REFERENCES Utilisateurs(IdUtilisateur)
 );
 
+CREATE TABLE Modules
+(
+    IdModule int NOT NULL AUTO_INCREMENT,
+    Nom varchar(20),
+    PRIMARY KEY(IdModule)
+);
+
 /* Creation de la table cours */
 CREATE TABLE Cours(
     IdCours int NOT NULL AUTO_INCREMENT,
